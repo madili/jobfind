786da2fb906ae69945c959c5c4cf919d6e081e5e
Marcos Dioni Lima <madili2016@gmail.com>
2015-10-16 16:23:39 +0000

    mongodb

---
+++ models/Job.js
@@ -0,0 +1,19 @@
+var mongoose = require("mongoose");
+
+var jobSchema = mongoose.Schema({
+    title: { type: String},
+    description: { type: String}
+});
+
+var Job = mongoose.model('Job', jobSchema);
+
+exports.seedJobs = function () {
+    Job.find({}).exec(function (error, collection) {
+        if(collection.length === 0) {
+            Job.create({ title: 'Cook', description: 'You will be making bagels'});
+            Job.create({ title: 'Enginner', description: 'You will be making areas'});
+            Job.create({ title: 'Developer', description: 'You will be making apps'});
+            Job.create({ title: 'Dress', description: 'You will be making customers'});
+        }
+    })
+}
--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 var express = require("express");
+var mongoose = require("mongoose");
+var jobModel = require("./models/Job")
 
 var app = express();
 
 app.set('views', __dirname);
 app.set('view engine', 'jade');
 
-
 app.use(express.static(__dirname + '/public'));
 
-
+app.get('/api/jobs', function(req, res) {
+   mongoose.model('Job').find({}).exec(function (error, collection) {
+       res.send(collection);
+   })
+});
 
 app.get('*', function (req, res) {
     res.render('index');
+});
+
+//mongoose.connect('mongodb://localhost/jobfinder');
+mongoose.connect('mongodb://psdev:123456@ds029297.mongolab.com:29297/jobfinder');
+
+
+
+var con = mongoose.connection;
+
+con.once('open', function() {
+    console.log('connected to mongodb successfully!')
+    jobModel.seedJobs();
 });
 
 app.listen(process.env.PORT, process.env.IP);
